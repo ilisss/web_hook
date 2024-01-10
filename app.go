ddfb79a7edcd415dac6524ae5609127d2aa1deba
ilisss <sidoroviliya@gmail.com>
2024-01-10 18:21:19 +0300

    my first normal app

--- app.go
+++ app.go
@@ -6,8 +6,29 @@ import (
 )
  
 func main(){
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w,"Hello world!")
-	})
-	http.ListenAndServe(":80",nil)
+	db:=database {"shoes":50,"socks":5}
+	http.HandleFunc("/list",db.list)
+	http.HandleFunc("/price",db.price)
+	http.ListenAndServe(":80", nil)
+}
+ type dollars float32
+func (d dollars) String() string {return fmt.Sprintf("$%.2f",d)}
+type database map[string]dollars
+
+func (db database) list (w http.ResponseWriter, req *http.Request){
+	for item,price :=range db{
+		fmt.Fprintf(w, "%s:,%s\n", item,price)
+	}
+}
+
+func(db database) price(w http.ResponseWriter, req *http.Request){
+	item:=req.URL.Query().Get("item")
+	price, ok:=db[item]
+
+	if!ok{
+		w.WriteHeader(http.StatusNotFound)//404
+		fmt.Fprintf(w,"no such item:%q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n",price)
 }
